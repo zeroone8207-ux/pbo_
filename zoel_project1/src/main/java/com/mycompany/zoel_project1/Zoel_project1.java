b966db0e4bf697bea25a2769afa3b717dfea1a56
zeroone8207-ux <zulfikar8207@gmail.com>
2026-09-02 10:26:08 +0000

    Add files via upload

---
+++ zoel_project1/src/main/java/com/mycompany/zoel_project1/Zoel_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zoel_project1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Zoel_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Muhammad Zulfikar Arkan");
+        System.out.println("NPM : 2505060024");
+        System.out.println("Alamat : Temanggung");
+        System.out.println("No_HP : 083830086767");
+    }
+}
